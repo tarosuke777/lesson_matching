45d1de956ce73a32dc4411f88d0a171fb2e95e6d
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-09-05 18:14:55 +0900

    teacher and admin controller add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :teachers
-  devise_for :admins
+  devise_for :teachers, controllers: {
+    sessions: 'teachers/sessions',
+    registrations: 'teachers/registrations'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
