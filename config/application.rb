8b4432d1aaa18fd68dc7723967dd8e22307d8751
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-09-26 16:56:40 +0900

    mod ft frames

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module LessonMatching
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
